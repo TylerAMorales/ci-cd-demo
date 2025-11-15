01987977961ecb5387b40ad1da79073c5eed1ff3
TylerAMorales <moralesrodionoff@gmail.com>
2025-11-14 18:04:03 -0800

    Initial commit

---
+++ test_main.py
@@ -0,0 +1,18 @@
+import unittest
+from main import add, multiply
+
+class TestMain(unittest.TestCase):
+    def test_add_positive(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_negative(self):
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_multiply_positive(self):
+        self.assertEqual(multiply(3, 4), 12)
+
+    def test_multiply_by_zero(self):
+        self.assertEqual(multiply(5, 0), 0)
+
+if __name__ == "__main__":
+    unittest.main()
