366f9a58f14723b0d4f85dd1f0db4e7f82c8fa21
TM <164290951+TylerAMorales@users.noreply.github.com>
2025-11-15 18:40:58 -0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -17,8 +17,8 @@ class TestMain(unittest.TestCase):
 if __name__ == "__main__":
     unittest.main()
 
-
 # Add this at the bottom of your file
 def hello_ci_cd():
     return "CI/CD pipeline works!"
+
 
